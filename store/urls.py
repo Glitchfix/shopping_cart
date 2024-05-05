0051abf9fb6aaadb0b5fea49fbc627277d5bdb57
Shivanjan <schakravorty846@gmail.com>
2024-05-06 01:03:42 +0530

    update: update URLs in store/urls.py
    Signed-off-by: Shivanjan <schakravorty846@gmail.com>

--- store/urls.py
+++ store/urls.py
@@ -8,6 +8,8 @@ from rest_framework_simplejwt.views import (
 
 
 urlpatterns = [
+    # path('swagger/', views.swagger.SwaggerView, name='swagger_doc'),
+    
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
@@ -16,15 +18,16 @@ urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
     
-    path('user/', views.UserView.as_view(), name='user'),
+    path('user/', views.UserView.as_view(), name='users'),
     path('user/<int:pk>/', views.UserView.as_view(), name='user'),
     
     path('product/', views.ProductView.as_view(), name='product'),
     
     path('cart/', views.CartView.as_view(), name='cart'),
     
-    path('payment/', views.PaymentView.as_view(), name='payment'),
+    path('payments/', views.PaymentView.as_view(), name='payment'),
+    path('payment/<int:payment_id>', views.PaymentView.as_view(), name='payment'),
     
-    path('orders/', views.OrderView.as_view(), name='order'),
-    
+    path('orders/', views.OrderView.as_view(), name='orders'),
+    path('order/<int:order_id>', views.OrderView.as_view(), name='order'),
 ]
